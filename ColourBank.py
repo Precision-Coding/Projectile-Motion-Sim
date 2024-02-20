1f205e38b3886ff1f02d137c200beaf821f17344
Callum Lewis <callumlewis852@gmail.com>
2024-02-20 21:29:34 +0000

    Adds more colours to bank (White black purple)

--- ColourBank.py
+++ ColourBank.py
@@ -1,5 +1,7 @@
 class Colour:
     def __init__(self):
+        self.black = (0,0,0)
+        self.white = (255,255,255)
         self.red = (255,0,0)
         self.green = (0,255,0)
         self.blue = (0,0,255)
